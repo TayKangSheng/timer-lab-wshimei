a1ca7be040a112a89a7804b7650441c00fc4d4f9
Shimei Wong <wshimei@gmail.com>
2017-01-26 11:07:53 +0800

    reset start pause buttons start working

--- timers.js
+++ timers.js
@@ -1,1 +1,28 @@
 // your code here:
+document.addEventListener('DOMContentLoaded', function () {
+  var reset = document.querySelector('#reset')
+  var start = document.querySelector('#start')
+  var pause = document.querySelector('#pause')
+
+  var seconds = 0
+  var timerId
+
+  function updateTime () {
+    seconds += 1
+    document.querySelector('#timer').innerHTML = 'Time elapsed: ' + seconds
+  }
+
+  start.addEventListener('click', function () {
+    timerId = setInterval(updateTime, 1000)
+  })
+
+  reset.addEventListener('click', function () {
+    clearInterval(timerId)
+    seconds = 0
+    document.querySelector('#timer').innerHTML = 'Stop Watch'
+  })
+
+  pause.addEventListener('click', function () {
+    clearInterval(timerId)
+  })
+})
